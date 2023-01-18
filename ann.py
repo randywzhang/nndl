e1f45ff756427e1e66ca71a557c6958abd0bfeeb
Randy Zhang <randywzhang@gmail.com>
2023-01-18 11:22:38 -0700

    Implement stochastic gradient descent algorithm

--- ann.py
+++ ann.py
@@ -1,16 +1,18 @@
 import numpy as np
-
-# TODO: stochastic implementation
+from keras.datasets import mnist
+import pickle
 
 """
 Basic ANN built by following Michael Nielsen's book Neural Networks and Deep Learning
+to classify handwritten digits from the MNIST dataset
 
 Network architecture : MLP, supervised learning
 """
-
-
 class Network(object):
 
+    """
+    Initializes network with random weights and biases taken from a normal distribution
+    """
     def __init__(self, sizes):
         self.num_layers = len(sizes)
         self.sizes = sizes
@@ -34,7 +36,6 @@ class Network(object):
     
     assumes valid input
     """
-
     def feed_forward(self, x):
         # The activation of each layer is f(z) where f is the sigmoid
         # function and z is the vector obtained by multiplying the
@@ -52,17 +53,12 @@ class Network(object):
     """
     Back propagation algorithm
     Determines the gradient of the loss function with respect to each 
-    parameter in the network and updates the weights by subtracting
-    the gradient scaled down by a learning rate η (eta).
+    parameter in the network
     
     @param x - input image
     @param y - expected output
     """
-
     def back_propagate(self, x, y):
-        # learning rate η (eta)
-        eta = 1e-5
-
         # derivatives of the loss function with respect to weights/biases
         dLdW = [np.zeros(w.shape) for w in self.weights]
         dLdb = [np.zeros(b.shape) for b in self.biases]
@@ -70,7 +66,6 @@ class Network(object):
         # feed forward and record activations and z values
         activations, z_values = self.feed_forward(x)
 
-        # TODO: calculate gradient
         # GRADIENT CALCULATION
 
         # begin with the derivative of the loss function with respect to the input
@@ -104,18 +99,88 @@ class Network(object):
         # return gradients of network variables
         return dLdb, dLdW
 
+    """
+    Stochastic gradient descent implementation 
+    Applies the backpropagation algorithm to the training data and updates 
+    the network variables by subtracting the gradient scaled down by a 
+    learning rate η (eta).
+    @param training_data - labeled mnist images used to train the network
+    @param epochs - number of times to repeatedly train on the training data
+    @param batch_size - the size of each batch of training data
+    @param learning_rate - the factor by which we scale the gradient down in 
+        order to prevent divergence from overshooting the minima
+    """
+    def SGD(self, training_data, epochs, batch_size, learning_rate, test_data=None):
+        # split training data into batches
+        batches = [training_data[i:i + batch_size] for i in range(0, len(training_data), batch_size)]
 
+        for epoch in range(epochs):
+            # shuffle the data, prevent overfitting
+            np.random.shuffle(training_data)
+
+            for batch in batches:
+                stochastic_b = [np.zeros(b.shape) for b in self.biases]
+                stochastic_W = [np.zeros(W.shape) for W in self.weights]
+
+                # aggregate the gradients from each data point
+                for training_datum in batch:
+                    dLdb, dLdW = self.back_propagate(training_datum[0], training_datum[1])
+
+                    # subtract the gradient because we want to minimize loss
+                    stochastic_b -= dLdb
+                    stochastic_W -= dLdW
+
+                # take the average of all of the gradients
+                stochastic_b /= len(batch)  # len(batch) is guaranteed to be batch_size for all batches except
+                stochastic_W /= len(batch)  # batches[-1] where it could be less than the batch_size
+
+                # scale down by the learning rate
+                stochastic_b *= learning_rate
+                stochastic_W *= learning_rate
+
+                # update the network variables
+                self.biases += stochastic_b
+                self.weights += stochastic_W
+
+            if test_data:
+                # TODO: visualize network variables here
+                # evaluate the network after every epoch
+                accuracy = self.evaluate(test_data)
+                print("Epoch " + epoch + ": " + accuracy)
+
+    """
+    Network evaluation function
+    Takes test data as input and evaluates the network based on percentage
+    of correct classifications
+    @param test_data - test dataset to feed forward through the network
+    @return - percentage of correctly classified images
+    """
+    def evaluate(self, test_data):
+        # keep track of correct classifications
+        num_correct = 0
+        for datum in test_data:
+            # send each input in a forward pass through the network
+            activations, z_values = self.feed_forward(datum[0])
+
+            # check that the activation of the final layer has its max value
+            # at the labeled digit
+            if max(activations[-1]) == activations[datum[1]]:
+                num_correct += 1
+
+        return num_correct / len(test_data)
+
+
+"""
+This network uses a quadratic loss function
+This function is unused in network training but is a good reference
+for learning purposes
+
+@param actual - network output for a given input x
+@param expected - labeled value for the given input x
+
+1/2 makes the derivatives nicer for backprop
+"""
 def loss(actual, expected):
-    """
-    This network uses a quadratic loss function
-    This function is unused in network training but is a good reference
-    for learning purposes
-
-    @param actual - network output for a given input x
-    @param expected - labeled value for the given input x
-
-    1/2 makes the derivatives nicer for backprop
-    """
     return np.square(np.linalg.norm(actual - expected)) / 2
 
 
@@ -125,21 +190,30 @@ def sigmoid(x):
 
 # The following functions are derivatives used for backpropagation
 def loss_derivative(actual, expected):
-    # TODO: loss(actual, expected) == loss(expected, actual)
-    # what happens when we return expected - actual?
-    # expected - actual = -1 (actual - expected)
-    # the partial derivatives change sign so the gradient also changes sign
-    #
-    # shouldn't it be np.linalg.norm(actual - expected) ??
-    # no, because the norm is a scalar and we want each weight to change based
-    # on how far from the expected value it is. We don't want each weight to
-    # change the same amount.
-    #
-    # So why is it actual - expected and not expected - actual?
     # (x - x0)^2 == (x0 - x)^2
-    # d/dx (x0 - x)^2 = 2 (x0 - x) * -1 <-- forgot this -1
+    # d/dx (x0 - x)^2 = 2 (x0 - x) * -1 == 2 (actual - expected)
     return actual - expected
 
 
 def sigmoid_prime(x):
+    # d/dx (1 / (1 + e^-x)) = e^-x / (1 + e^-x)^2
+    #                       = (e^-x + 1 - 1) / (1 + e^-x)^2
+    #                         factor out 1 / (1 + e^-x)
+    #                       = [1 / (1 + e^-x)] * [(1 + e^-x - 1) / (1 + e^-x)]
+    #                       = sigmoid(x) * [(1 + e^-x) / (1 + e^-x) - 1 / (1 + e^-x)]
+    #                       = sigmoid(x) * (1 - sigmoid(x))
     return sigmoid(x) * (1 - sigmoid(x))
+
+
+# TODO: implement saving and loading
+"""
+Utility functions for saving and loading a network
+"""
+def save_network(ann, filename):
+    with open(filename, 'wb') as out_file:
+        pickle.dump(ann, out_file, pickle.HIGHEST_PROTOCOL)
+
+
+def load_network(filename):
+    with open(filename, 'rb') as in_file:
+        return pickle.load(in_file)
